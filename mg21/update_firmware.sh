e7a7137c5b0fb1a7baad633d98467b86dede45fc
Csongor Zalatnai <csongor@zalatnai.hu>
2024-07-04 02:08:44 +0200

    Create update_firmware.sh

---
+++ mg21/update_firmware.sh
@@ -0,0 +1,4 @@
+curl -s -k -L -o /tmp/firmware.gbl https://raw.githubusercontent.com/zalatnaicsongor/MGLGWFW/main/mg21/ncp-uart-hw-7430-115200-nohw.gbl
+zigbee_isp.sh 0 && usleep 20000 && zigbee_reset.sh 1 && usleep 20000 && zigbee_reset.sh 0 && usleep 20000
+echo -e '1' > /dev/ttyS1
+sx -X /tmp/firmware.gbl </dev/ttyS1 >/dev/ttyS1
