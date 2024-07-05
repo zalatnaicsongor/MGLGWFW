7d04e7f7a87532509a176593fc82b3e13ac40c38
Csongor Zalatnai <csongor@zalatnai.hu>
2024-07-05 02:14:16 +0200

    Update update_firmware.sh

--- mg21/update_firmware.sh
+++ mg21/update_firmware.sh
@@ -1,4 +1,4 @@
-curl -s -k -L -o /tmp/firmware.gbl https://github.com/Nerivec/silabs-firmware-builder/releases/download/v8.0.0-pre/easyiot_zb-gw04-1v1_ncp-uart-sw_115200_8.0.0.0.gbl
+curl -s -k -L -o /tmp/firmware.gbl https://raw.githubusercontent.com/zalatnaicsongor/MGLGWFW/main/mg21/ncp-uart-hw-8000-noflowcontrol-115200.gbl
 zigbee_isp.sh 0 && usleep 20000 && zigbee_reset.sh 1 && usleep 20000 && zigbee_reset.sh 0 && usleep 20000
 echo -e '1' > /dev/ttyS1
 sx -X /tmp/firmware.gbl </dev/ttyS1 >/dev/ttyS1
